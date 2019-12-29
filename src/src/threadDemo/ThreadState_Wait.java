32c0aecb76d072c43a8704ec19256c5791bdd41b
lazy_mac <627770537@qq.com>
2019-12-29 14:58:03 +0800

    Thread

--- src/src/threadDemo/ThreadState_Wait.java
+++ src/src/threadDemo/ThreadState_Wait.java
@@ -12,7 +12,7 @@ public class ThreadState_Wait {
                     obj.wait(3000L);
                     System.out.println("t1将wait");
                     obj.wait();
-                    System.out.println("t1 将 执行完");
+                    System.out.println("t1执行完毕");
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -20,8 +20,22 @@ public class ThreadState_Wait {
         });
 
         t1.start();
+        //确保t1进入wait(3000L)方法
         Thread.sleep(1000L);
 
         System.out.println("t1执行t1将wait（3000L）时候的状态："+ t1.getState());
+        //确保t1进入wait()方法
+        Thread.sleep(3000L);
+
+        System.out.println("t1执行wait()方法时候的状态:"+t1.getState());
+
+        //释放锁资源
+        synchronized (obj){
+            obj.notify();
+        }
+        System.out.println("t1.notify()后的状态"+t1.getState());
+
+        Thread.sleep(1000L);//确保t1能运行结束，不做此操作，t1可能会出 BLOCKED...
+        System.out.println("t1后的状态"+t1.getState());
     }
 }

0c2426d5108283fdf95bf559476b30aa797cb2ed
sunhd_windows <627770537@qq.com>
2020-04-08 21:07:47 +0800

    windows modify

--- src/src/threaddemo/volatiledemo/Maintest.java
+++ src/src/threaddemo/volatiledemo/Maintest.java
@@ -1,32 +1,32 @@
 package src.threaddemo.volatiledemo;
 
 public class Maintest {
-    public static volatile int race = 0;
+public static volatile int race = 0;
 
-    public static void increase() {
+public static void increase() {
         race++;
-    }
+        }
 
-    public static synchronized void increase1() {
+public static synchronized void increase1() {
         race++;
-    }
+        }
 
-    private static final int THREADS_COUNT = 20;
+private static final int THREADS_COUNT = 20;
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
 
         Thread[] threads = new Thread[THREADS_COUNT];
         for (int i = 0; i < THREADS_COUNT; i++) {
-            threads[i] = new Thread(() -> {
-                for (int j = 0; j < 10000; j++) {
-                    increase1();
-                }
-            });
-            threads[i].start();
+        threads[i] = new Thread(() -> {
+        for (int j = 0; j < 10000; j++) {
+        increase1();
+        }
+        });
+        threads[i].start();
         }
         while (Thread.activeCount() > 1) {
-            Thread.yield();
+        Thread.yield();
         }
         System.out.println(race);
-    }
-}
+        }
+        }
